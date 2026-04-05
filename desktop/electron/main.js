5143e0b279064eb5e67c6432c98e850f2b6f6826
jackystormer <ton@email.com>
2026-04-05 08:12:59 +0200

    fix crypto.subtle undefined on Windows

--- desktop/electron/main.js
+++ desktop/electron/main.js
@@ -13,6 +13,10 @@ const path = require('path');
 const isWin = process.platform === 'win32';
 const isMac = process.platform === 'darwin';
 
+if (!globalThis.crypto) {
+  globalThis.crypto = require('crypto').webcrypto;
+}
+
 function UpsertKeyValue(obj, keyToChange, value) {
   const keyToChangeLower = keyToChange.toLowerCase();
   for (const key of Object.keys(obj)) {
@@ -97,11 +101,6 @@ function createWindow() {
     win = null;
   });
 
-  // Open the DevTools.
-  // win.webContents.openDevTools();
-  // const log = require('electron-log/main');
-  // log.initialize();
-
   win.on('focus', () => {
     win.webContents.send('focused');
   });
@@ -139,12 +138,10 @@ const defaultMenuOptions = [
         },
       ]
     : []),
-  // { role: 'fileMenu' }
   {
     label: 'File',
     submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
   },
-  // { role: 'editMenu' }
   {
     label: 'Edit',
     submenu: [
@@ -168,7 +165,6 @@ const defaultMenuOptions = [
         : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
     ],
   },
-  // { role: 'viewMenu' }
   {
     label: 'View',
     submenu: [
@@ -183,7 +179,6 @@ const defaultMenuOptions = [
       { role: 'togglefullscreen' },
     ],
   },
-  // { role: 'windowMenu' }
   {
     label: 'Window',
     submenu: [
@@ -245,30 +240,18 @@ const menu = Menu.buildFromTemplate([
 ]);
 Menu.setApplicationMenu(menu);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
